16f9ed0dc96043a274497e81c9b1f3cb59777624
Evan Jamaq <xevan19@gmail.com>
2024-11-09 01:02:06 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./src/**/*.{html,js},",
+    "./src/**/*.{html,js}",
     "node_modules/preline/dist/*.js",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('tailgrids/plugin'),
+  plugins: [
+    require('tailgrids/plugin'),
     require('flowbite/plugin'),
     require('preline/plugin'),
   ],
